0683e2eebdddb3f121a41ac072fa7507050075e5
adoyle2014 <adam@doyleware.com>
2015-04-09 03:46:43 +0000

    create style and ingredients interface

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
     before_action :set_recipe, only: [:edit, :update, :show, :like]
-    before_action :require_user, except: [:show, :index]
+    before_action :require_user, except: [:show, :index, :like]
+    before_action :require_user_like, only: [:like]
     before_action :require_same_user, only: [:edit, :update]
 
     def index
@@ -40,6 +41,13 @@ class RecipesController < ApplicationController
         end
     end
     
+    def require_user_like
+        if !logged_in?
+          flash[:danger] = "You must be logged in to perform this action"
+          redirect_to :back
+        end
+    end
+    
     def like
         like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
         if like.valid?
@@ -51,10 +59,12 @@ class RecipesController < ApplicationController
         end
     end
     
+    
+    
         private
             
             def recipe_params
-                params.require(:recipe).permit(:name, :summary, :description, :picture)
+                params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids:[])
             end
             
             def set_recipe
